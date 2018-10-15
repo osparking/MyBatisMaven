ce4ee0278da2dfcaca9d8353b3456be7e4a498e1
Park,JongBum <jbpark03@gmail.com>
2018-10-15 09:25:05 +0900

    Ch 02-MoBatis(Core) Ex 1. step 1 thru step 4.
    To prepare CASE 1. single domain object.

---
+++ src/com/learning/spring/db/mybatis/PetDVO.java
@@ -0,0 +1,71 @@
+package com.learning.spring.db.mybatis;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class PetDVO implements Serializable {
+	private Integer id;
+	private String name;
+	private String owner;
+	private String species;
+	private String sex;
+	private Date birth;
+	private Date death;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public Date getDeath() {
+		return death;
+	}
+
+	public void setDeath(Date death) {
+		this.death = death;
+	}
+
+}
---
+++ src/com/learning/spring/db/mybatis/core/CoreMyBatisMain.java
@@ -0,0 +1,38 @@
+﻿package com.learning.spring.db.mybatis.core;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.learning.spring.db.mybatis.PetDVO;
+
+public class CoreMyBatisMain {
+
+	public static void main(String[] args) {
+		try {
+			CoreMyBatisMain main = new CoreMyBatisMain();
+
+			// Printing pets data
+			List<PetDVO> allPets = main.getAllPetsData();
+			System.out.println("--- 애완동물 숫자 ----" + allPets.size());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private static SqlSession getSqlSession() throws Exception {
+		String resource = "core-mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		return sqlSessionFactory.openSession();
+	}
+
+	public List<PetDVO> getAllPetsData() throws Exception {
+		return getSqlSession().selectList("getAllPets");
+	}
+}
