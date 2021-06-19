f3bef8ba903a152fc5e8e4d7f64f8262ae6d6275
Park,JongBum <jbpark03@gmail.com>
2021-06-19 21:05:57 +0900

    Util-getY4MMDD-method-added

---
+++ src/com/learning/Util.java
@@ -0,0 +1,24 @@
+package com.learning;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	final static DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	
+	public static String getY4MMDD(Date d) {
+		String date = sdf.format(d);
+		return date;
+	}
+	
+	public static Date getDate(String dateString) {
+		Date diedOn = null;
+		try {
+			diedOn = sdf.parse(dateString);
+		} catch (java.text.ParseException e) {
+			e.printStackTrace();
+		}
+		return diedOn;
+	}	
+}
--- src/com/learning/spring/db/mybatis/core/CoreMyBatisMain.java
+++ src/com/learning/spring/db/mybatis/core/CoreMyBatisMain.java
@@ -15,6 +15,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.learning.Util;
 import com.learning.spring.db.PetDVO;
 
 public class CoreMyBatisMain {
