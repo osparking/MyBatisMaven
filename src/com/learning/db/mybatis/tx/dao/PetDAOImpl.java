8197a520d6a4e20380d7bf4db72a85a361fbf7b7
Park,JongBum <jbpark03@gmail.com>
2021-06-26 18:32:24 +0900

    Chap-5-Programmagic-Transaction-Ex-2-Done

--- src/com/learning/db/mybatis/tx/dao/PetDAOImpl.java
+++ src/com/learning/db/mybatis/tx/dao/PetDAOImpl.java
@@ -4,17 +4,19 @@ import java.util.Date;
 import java.util.HashMap;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.learning.Util;
 import com.learning.spring.db.PetDVO;
-import org.springframework.jdbc.core.JdbcTemplate;
-import java.util.*;
-import java.sql.Types;
 
+@Component
 public class PetDAOImpl implements PetDAO {
 	private SqlSessionTemplate sqlSessionTemplate;
+	private TransactionTemplate transactionTemplate;
 
 	@Override
 	public void doInsertAndUpdateInTx() {
@@ -40,10 +42,37 @@ public class PetDAOImpl implements PetDAO {
 	}
 
 	@Override
+	public void doInsertAndUpdateUsingTxTemplate() {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				try {
+					/*
+					 * 애완동물 삽입
+					 */
+					insertPet();
+					/*
+					 * 의도적 예외 유발
+					 */
+					int i = 0;
+					int j = 100 / i;
+					/*
+					 * 애완동물 갱신
+					 */
+					updatePetData();
+				} catch (Exception ex) {
+					String res = Util.inKoean(status.isCompleted());
+					System.out.println("** 삽입&갱신(prog) 성공인가? : " + res);
+					status.setRollbackOnly();
+				}
+			}
+		});
+	}
+
+	@Override
 	public void insertPet() {
 		// Data to be inserted
 		PetDVO petDVO = new PetDVO();
-		petDVO.setName("Slimmy");
+		petDVO.setName("사사삭");
 		petDVO.setOwner("John");
 		petDVO.setSpecies("Snake");
 		petDVO.setSex("f");
@@ -64,7 +93,7 @@ public class PetDAOImpl implements PetDAO {
 		 * 뱀 이름을 바꾼다.
 		 */
 		PetDVO petDVO = new PetDVO();
-		petDVO.setName("Slimmy");
+		petDVO.setName("살며사");
 		petDVO.setSex("f");
 		petDVO.setBirth(new Date());
 		HashMap<String, Object> inputMap = new HashMap<String, Object>();
@@ -72,6 +101,10 @@ public class PetDAOImpl implements PetDAO {
 		inputMap.put("sex", petDVO.getSex());
 		inputMap.put("name", petDVO.getName());
 		sqlSessionTemplate.update("updatePetData", inputMap);
+	}
+
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
 	}
 
 	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
